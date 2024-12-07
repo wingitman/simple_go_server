ed9a60eb716359f0c8f3f8c1d4e025b62d67fb9e
wingitman <dlblackburn111@gmail.com>
2024-12-07 03:20:58 +0000

    init simple server
    simple server with TLS

---
+++ server.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "log"
+    "net/http"
+)
+
+func main() {
+
+    http.Handle("/", http.FileServer(http.Dir("./static")))
+
+    log.Fatal(http.ListenAndServeTLS(":443", "server.crt", "server.key", nil))
+}
